44484cf0b795eb135588271eb920137ed4f88cfd
Edgar <edgaarr2005@hotmail.com>
2025-07-30 07:36:21 -0400

    B1. Created a user option to enter, update and delete vacations.

--- app/src/main/java/com/example/vacationscheduler/UI/MainActivity.java
+++ app/src/main/java/com/example/vacationscheduler/UI/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.vacationscheduler;
+package com.example.vacationscheduler.UI;
 
 import android.os.Bundle;
 
@@ -8,8 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.content.Intent;
 
+import com.example.vacationscheduler.R;
+
+public class MainActivity extends AppCompatActivity {
+    public static int numAlert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void goToVacationList (View view) {
+        Intent intent = new Intent(this, VacationList.class);
+        startActivity(intent);
     }
 }
