a9f541b95b390619cdedeb3b6efcd9c8c26d5554
Edgar <edgaarr2005@hotmail.com>
2025-07-30 07:48:30 -0400

    B4. Included the corresponding details for each excursion.

---
+++ app/src/main/java/com/example/vacationscheduler/DAO/ExcursionDAO.java
@@ -0,0 +1,30 @@
+package com.example.vacationscheduler.DAO;
+
+import com.example.vacationscheduler.entities.Excursion;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface ExcursionDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Excursion excursion);
+
+    @Update
+    void update(Excursion excursion);
+
+    @Delete
+    void delete(Excursion excursion);
+
+    @Query("SELECT * FROM excursions ORDER BY id ASC")
+    List<Excursion> getAllExcursions();
+
+    @Query("SELECT * FROM excursions WHERE vacationId = :vacationId ORDER BY id ASC")
+    List<Excursion> getAssociatedExcursions(int vacationId);
+}
