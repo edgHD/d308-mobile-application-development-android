7773c1f2dcda764d5c6a581e7a40d439b31d3a03
Edgar <edgaarr2005@hotmail.com>
2025-07-30 07:38:30 -0400

    B2. Included the corresponding details for each vacation.

---
+++ app/src/main/java/com/example/vacationscheduler/DAO/VacationDAO.java
@@ -0,0 +1,27 @@
+package com.example.vacationscheduler.DAO;
+
+import com.example.vacationscheduler.entities.Vacation;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface VacationDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Vacation vacation);
+
+    @Update
+    void update(Vacation vacation);
+
+    @Delete
+    void delete(Vacation vacation);
+
+    @Query("SELECT * FROM vacations ORDER BY id ASC")
+    List<Vacation> getAllVacations();
+}
